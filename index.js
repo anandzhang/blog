2a5941bb9b0feb0f7e91ada1a9d0a1597aa58484
anandzhang <anandzhangnew@gmail.com>
2020-02-01 19:11:52 +0800

    添加博客主页

---
+++ index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const port = 3000
+
+// 静态资源
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  fs.readFile('views/index.html', (err, data) => {
+    if (err) return res.status(500).send('index.html error')
+    res.send(data.toString())
+  })
+})
+
+app.listen(port, () => {
+  console.log(`app is running on the http://localhost:${port}`)
+})
