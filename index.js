b6648332051cbab189b78eb4d9f7d0ed566dc50c
张中元 <33146742+anandzhang@users.noreply.github.com>
2020-02-01 22:02:32 +0800

    创建文章界面 (#4)

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get('/', (req, res) => {
   })
 })
 
+app.get('/posts', (req, res) => {
+  fs.readFile('views/posts.html', (err, data) => {
+    if (err) return res.status(500).send('posts.html error')
+    res.send(data.toString())
+  })
+})
+
 app.listen(port, () => {
   console.log(`app is running on the http://localhost:${port}`)
 })
