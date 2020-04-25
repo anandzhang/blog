28917e9534a6fa2100d8ee79161487497d7565a0
anandzhang <anandzhangnew@gmail.com>
2020-04-25 18:04:45 +0800

    更换模板引擎package

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = express()
 const port = 3000
 
 // 配置模板引擎
-app.engine('html', require('express-art-template'))
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'pug');
 
 // 静态资源
 app.use('/public', express.static(path.join(__dirname, 'public')))
