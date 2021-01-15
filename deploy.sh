dcddf7d542f1df43ad5bf421f47ecb714c204836
Anand <33146742+anandzhang@users.noreply.github.com>
2021-01-15 16:14:46 +0800

    添加TypeScript、pm2工具变为全局包 (#80)

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,17 @@
 BASE_DIR=$(dirname "$0")
 cd ${BASE_DIR}
 
+if !(command -v pm2 >/dev/null 2>&1); then 
+  yarn global add --silent pm2
+  # pm2 install typescript
+fi
+
 APP_NAME=blog
 git pull
 yarn install --silent
 yarn build
 
-if [ "$(yarn --silent pm2 id ${APP_NAME})" = "[]" ]; then
+if [ "$(pm2 id ${APP_NAME})" = "[]" ]; then
   yarn start
-  yarn pm2 save
+  pm2 save
 fi
