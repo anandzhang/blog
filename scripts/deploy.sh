33457d9a863fa6fc685e0c22e1520e68bb45affd
anandzhang <anandzhangnew@gmail.com>
2021-01-20 13:59:22 +0800

    添加新的部署脚本运行方式

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-BASE_DIR=$(dirname "$0")
-cd ${BASE_DIR}
+
+readonly APP_NAME=blog
+readonly SERVER_FILE=build/index.js
 
 if !(command -v pm2 >/dev/null 2>&1); then 
   yarn global add --silent pm2
   # pm2 install typescript
 fi
 
-APP_NAME=blog
-git pull
-yarn install --silent
+cd ${APP_NAME}
+# git pull
+# yarn install --silent
 yarn build
+yarn addPost
 
 if [ "$(pm2 id ${APP_NAME})" = "[]" ]; then
-  yarn start
+  pm2 start ${SERVER_FILE} -n ${APP_NAME} --watch
   pm2 save
 fi
