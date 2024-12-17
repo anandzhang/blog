69b8cdf71bfba615f0027f1c4e58fdce477969c3
anandzhang <hi@anandzhang.com>
2024-12-17 20:25:48 +0800

    chore: move to ECS, rebuild

--- utils/addPost.ts
+++ utils/addPost.ts
@@ -1,37 +1,38 @@
 import fs from 'fs/promises'
 import path from 'path'
-import mongoose from 'mongoose'
-import Post from '../models/Post'
-import Database from './Database'
-import MarkdownParser from './MarkdownParser'
+// import mongoose from 'mongoose'
+// import Post from '../models/Post'
+// import Database from './Database'
+// import MarkdownParser from './MarkdownParser'
 
-Database.connect(async () => {
-  await Post.deleteMany({})
-
-  try {
-    const files = await readFileInDir('posts')
-    for (const file of files) {
-      const [no, title] = parseFilename(file.name)
-      const category = path.basename(file.directory)
-      const requestPath = path.resolve('/', file.directory, no)
-      const mdParser = new MarkdownParser(file.path)
-      const fields = mdParser.parseYAMLFrontMatter()
-      const content = mdParser.parseContent(requestPath)
-      await Post.create({
-        no,
-        title,
-        category,
-        requestPath,
-        content,
-        ...fields
-      })
-    }
-  } catch (error) {
-    console.log(error)
-  }
-
-  mongoose.disconnect()
-})
+// v2 未使用，暂时去掉
+// Database.connect(async () => {
+//   await Post.deleteMany({})
+//
+//   try {
+//     const files = await readFileInDir('posts')
+//     for (const file of files) {
+//       const [no, title] = parseFilename(file.name)
+//       const category = path.basename(file.directory)
+//       const requestPath = path.resolve('/', file.directory, no)
+//       const mdParser = new MarkdownParser(file.path)
+//       const fields = mdParser.parseYAMLFrontMatter()
+//       const content = mdParser.parseContent(requestPath)
+//       await Post.create({
+//         no,
+//         title,
+//         category,
+//         requestPath,
+//         content,
+//         ...fields
+//       })
+//     }
+//   } catch (error) {
+//     console.log(error)
+//   }
+//
+//   mongoose.disconnect()
+// })
 
 interface IFile {
   name: string,
